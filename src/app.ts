dc5af42175fb098a87976a8f1cd241401d401b61
Erick Rodrigues <erickrodrigues3975@gmail.com>
2025-03-07 12:39:07 -0300

    docs: created complete documentation

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import swaggerUi from 'swagger-ui-express'
+import swaggerDocument from '../swagger.json'
 
 export const app = express()
 
 app.use(cors())
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 app.use(express.json())
